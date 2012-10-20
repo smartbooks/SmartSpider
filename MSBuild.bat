84b84d8a6f490dc6c3069728bc61fca5a1c762c3
王亚 <smartbooks@qq.com>
2012-10-20 17:14:01 +0800

    modify msbuild config file.

--- MSBuild.bat
+++ MSBuild.bat
@@ -1,2 +1,21 @@
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe SmartSpider10.sln /t:Rebuild
-pause
+
+ECHO off
+
+c:\windows\Microsoft.Net\Framework\v3.5\MSBuild SmartSpider10.sln /t:Rebuild
+
+del SmartSpider10.sln.cache
+del /s SmartSpider\bin\Smart*.pdb
+del /s SmartSpider\bin\Smart*.xml
+del /s SmartDBUtility\bin\Smart*.pdb
+del /s SmartDBUtility\bin\Smart*.xml
+del /s SmartDBUtility\bin\Smart*.dll
+rd /s /q SmartDBUtility\obj
+rd /s /q SmartSecurity\bin
+rd /s /q SmartSecurity\obj
+rd /s /q SmartSpiderConfig\bin
+rd /s /q SmartSpiderConfig\obj
+rd /s /q SmartSpiderIUtility\bin
+rd /s /q SmartSpiderIUtility\obj
+rd /s /q SmartUtility\bin
+rd /s /q SmartUtility\obj
+rd /s /q SmartSpider\obj
--- readme.txt
+++ readme.txt
@@ -3,3 +3,4 @@
 1.成功编译这个解决方案至少需要安装.Net framework 3.5 sp1.
   下载地址:http://download.microsoft.com/download/2/0/E/20E90413-712F-438C-988E-FDAA79A8AC3D/dotnetfx35.exe
 2.如果未安装Visual Studio开发工具,运行MSBuild.bat批处理也可以编译项目文件.
+3.应用程序启动位置:SmartSpider\bin\Debug\SmartSpider.exe
